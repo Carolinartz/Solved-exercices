87a0e76200bbf7cf5e87fcdd7f64c9ceae081938
carolina <carolinartz5991@gmail.com>
2024-07-21 22:30:41 -0500

    feat:FunctionHelloName

---
+++ GYMDOS/HelloName.js
@@ -0,0 +1,5 @@
+//Implementa la función helloName para que dado un string name, 
+//por ejemplo "Bob", retorne un saludo de la forma "Hello Bob!".
+function HelloName(name){
+    return `Hello${name}!`;
+}
