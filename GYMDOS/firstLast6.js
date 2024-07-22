d53c4f0917d545e8f65d8702449d461734874e75
carolina <carolinartz5991@gmail.com>
2024-07-21 22:48:57 -0500

    feat:firtslast6

---
+++ GYMDOS/firstLast6.js
@@ -0,0 +1,7 @@
+//Implementa la función firstLast6 que recibe un arreglo con números y retorne true si el número 6 es el primer o el último elemento del arreglo.
+// El arreglo tendrá 1 o más elementos.
+//funcion 
+//retorno condicional o
+Function(nums){
+    return (nums[0]===6||nums[nums.length-1]===6);
+}
